b4b84e35054d70dddec8e8de6c54088a3a43dcf5
sebbu <zsbe17fr@yahoo.fr>
2013-06-17 00:54:41 +0200

    Updated pastes & update script

--- update.php
+++ update.php
@@ -12,23 +12,23 @@ $html_data.='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "htt
 $data=file_get_contents('https://api.github.com/users/sebbu2/repos');
 $ar=json_decode($data);
 if(count($ar)>0) {
-	$html_data.='<table border="1">'."\r\n";
+	$html_data.='<table border="1">'."\n";
 	foreach($ar as $elem) {
-		$html_data.='	<tr>'."\r\n";
-		$html_data.='		<td><a href="'.$elem->html_url.'">'.$elem->name.'</a></td>'."\r\n";
+		$html_data.='	<tr>'."\n";
+		$html_data.='		<td><a href="'.$elem->html_url.'">'.$elem->name.'</a></td>'."\n";
 		if(strlen($elem->description)>0) {
-			$html_data.='		<td>'.$elem->description.'</td>'."\r\n";
+			$html_data.='		<td>'.$elem->description.'</td>'."\n";
 		}
 		else {
-			$html_data.='		<td>&#x200B;</td>'."\r\n";
+			$html_data.='		<td>&#x200B;</td>'."\n";
 		}
 		if(strlen($elem->language)>0) {
-			$html_data.='		<td>'.$elem->language.'</td>'."\r\n";
+			$html_data.='		<td>'.$elem->language.'</td>'."\n";
 		}
 		else {
-			$html_data.='		<td>&#x200B;</td>'."\r\n";
+			$html_data.='		<td>&#x200B;</td>'."\n";
 		}
-		$html_data.='	</tr>'."\r\n";
+		$html_data.='	</tr>'."\n";
 	}
 	$html_data.='</table>';
 }
