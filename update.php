6da7fd61a430b8ca3318b7b21209603e146fb30b
sebbu <514794+sebbu2@users.noreply.github.com>
2025-08-28 21:52:37 +0200

    update pastes & repositories. support > per_page repositories, fix EOL

--- update.php
+++ update.php
@@ -9,20 +9,28 @@ $html_data.='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "htt
 <body>
 
 ';
-$data=file_get_contents('https://api.github.com/users/sebbu2/repos');
+$num=1;
+$data=file_get_contents('https://api.github.com/users/sebbu2/repos?per_page=100');
 $ar=json_decode($data);
-if(count($ar)>0) {
+$ar2=$ar;
+while(count($ar)==100) {
+	$num++;
+	$data=file_get_contents('https://api.github.com/users/sebbu2/repos?per_page=100&page='.$num);
+	$ar=json_decode($data);
+	$ar2+=$ar;
+}
+if(count($ar2)>0) {
 	$html_data.='<table border="1">'."\n";
-	foreach($ar as $elem) {
+	foreach($ar2 as $elem) {
 		$html_data.='	<tr>'."\n";
 		$html_data.='		<td><a href="'.$elem->html_url.'">'.$elem->name.'</a></td>'."\n";
-		if(strlen($elem->description)>0) {
+		if(!is_null($elem->description)&&strlen($elem->description)>0) {
 			$html_data.='		<td>'.$elem->description.'</td>'."\n";
 		}
 		else {
 			$html_data.='		<td>&#x200B;</td>'."\n";
 		}
-		if(strlen($elem->language)>0) {
+		if(!is_null($elem->language)&&strlen($elem->language)>0) {
 			$html_data.='		<td>'.$elem->language.'</td>'."\n";
 		}
 		else {
